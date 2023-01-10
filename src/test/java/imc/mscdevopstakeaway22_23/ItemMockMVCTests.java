13be48ed91396a263b98aeda981861753f93f42b
scmimc <cooperim@cardiff.ac.uk>
2023-01-10 18:02:06 +0000

    MockMVC full container testing for Home page (no data)

---
+++ src/test/java/imc/mscdevopstakeaway22_23/ItemMockMVCTests.java
@@ -0,0 +1,31 @@
+package imc.mscdevopstakeaway22_23;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ItemMockMVCTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Unit tests using a mock MVC
+    @Test
+    public void a_testGreeting() throws Exception {
+        this.mockMvc.perform(get("/Home")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("HomePage")));
+    }
+
+
+}
+
