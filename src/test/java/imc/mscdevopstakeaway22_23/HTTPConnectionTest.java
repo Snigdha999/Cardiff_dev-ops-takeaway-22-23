0ba4231275919d1aaa69fde41f63e20798bd07a6
scmimc <cooperim@cardiff.ac.uk>
2023-01-10 17:17:51 +0000

    Full context and server test

---
+++ src/test/java/imc/mscdevopstakeaway22_23/HTTPConnectionTest.java
@@ -0,0 +1,28 @@
+package imc.mscdevopstakeaway22_23;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HTTPConnectionTest {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/Hello.html",
+                String.class)).contains("Hello2");
+    }
+    
+}
