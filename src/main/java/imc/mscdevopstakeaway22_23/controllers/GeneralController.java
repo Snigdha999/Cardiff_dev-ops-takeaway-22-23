aa87543819dd95790845b284263ec06cabcc7c2c
scmimc <cooperim@cardiff.ac.uk>
2023-01-10 12:10:53 +0000

    Form data extracted and printed to server(tmp)

--- src/main/java/imc/mscdevopstakeaway22_23/controllers/GeneralController.java
+++ src/main/java/imc/mscdevopstakeaway22_23/controllers/GeneralController.java
@@ -2,12 +2,18 @@ package imc.mscdevopstakeaway22_23.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import imc.mscdevopstakeaway22_23.DTO.ItemDTO;
+import imc.mscdevopstakeaway22_23.Form.AddItemForm;
+
 import imc.mscdevopstakeaway22_23.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -42,10 +48,23 @@ public class GeneralController {
         return mav;
     }
 
-    @RequestMapping(path="/Admin/AddItem")
-    public ModelAndView A_AddItem(){
+    @RequestMapping(path="/Admin/AddItem", method = RequestMethod.GET)
+    public ModelAndView A_AddItemGET(){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("Admin/AddItem");
+        return mav;
+    }
+
+    @RequestMapping(path="/Admin/AddItem", method = RequestMethod.POST)
+    public ModelAndView A_AddItemPOST(AddItemForm item, BindingResult br){
+        ModelAndView mav;
+        if (br.hasErrors()) {
+            System.out.println(br.getFieldErrors().toString());
+            mav = A_AddItemGET();
+        } else {
+            mav = menu();
+        }
+        System.out.println(item.getName());
         return mav;
     }
 }
