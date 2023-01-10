4d41765a0453992825c90f94ad359ba2798e1f00
scmimc <cooperim@cardiff.ac.uk>
2023-01-10 09:59:10 +0000

    Initial initializr project - not running

---
+++ src/main/java/imc/mscdevopstakeaway22_23/Mscdevopstakeaway2223Application.java
@@ -0,0 +1,13 @@
+package imc.mscdevopstakeaway22_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mscdevopstakeaway2223Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mscdevopstakeaway2223Application.class, args);
+    }
+
+}
---
+++ src/test/java/imc/mscdevopstakeaway22_23/Mscdevopstakeaway2223ApplicationTests.java
@@ -0,0 +1,13 @@
+package imc.mscdevopstakeaway22_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mscdevopstakeaway2223ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
