11a180e15a18c61fa6b70f17e03b74a329832080
scmimc <cooperim@cardiff.ac.uk>
2023-01-10 19:31:41 +0000

    Lightweight MockMVC Test - mocked data call using "given"

--- src/test/java/imc/mscdevopstakeaway22_23/LightweightMockMVCTests.java
+++ src/test/java/imc/mscdevopstakeaway22_23/LightweightMockMVCTests.java
@@ -1,6 +1,7 @@
 package imc.mscdevopstakeaway22_23;
 
 
+import imc.mscdevopstakeaway22_23.DTO.ItemDTO;
 import imc.mscdevopstakeaway22_23.controllers.GeneralController;
 
 import imc.mscdevopstakeaway22_23.repository.ItemRepository;
@@ -12,12 +13,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GeneralController.class)
@@ -36,4 +41,19 @@ public class LightweightMockMVCTests {
         this.mockMvc.perform(get("/Home")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("HomePage")));
     }
+
+    //  Component? test using a mockMVC but mocking the repository response (Note the @MockBean)
+    //  This only tests the controller and the view layers
+    @Test
+    public void getMenuTest() throws Exception {
+
+        ItemDTO item = new ItemDTO("mockDBChips", "desc", 3,2);
+
+        // This will return what the ItemRepo would have returned - a List of ItemDTOs
+        given(this.ItemRepo.getAllItems()).willReturn(Arrays.asList(item));
+
+        this.mockMvc.perform(get("/Menu")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("mockDBChips")));
+    }
+
 }
